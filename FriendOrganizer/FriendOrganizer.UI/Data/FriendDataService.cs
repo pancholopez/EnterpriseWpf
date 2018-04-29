96095b5f34813eca9214fafcfc7f6f700e79c48c
Francisco Lopez <pancholopez@gmail.com>
2018-04-29 20:48:43 +0200

    created initial dataservice stub

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            //todo: load from database
+            yield return new Friend { FirstName = "Francisco", LastName = "Lopez" };
+            yield return new Friend { FirstName = "Irma", LastName = "Estrada" };
+            yield return new Friend { FirstName = "Christopher", LastName = "Lopez" };
+            yield return new Friend { FirstName = "Fidel", LastName = "Razo" };
+        }
+    }
+}
